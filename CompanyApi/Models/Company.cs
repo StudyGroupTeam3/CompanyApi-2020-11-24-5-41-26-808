e3f77f4aaf209c57a067942f6d5cf0a4c39e6d89
Ziyuan <zxu25@slb.com>
2020-11-24 20:46:15 +0800

    feat: US1 AC1 POST /company add AddCompany() to create and add new company

---
+++ CompanyApi/Models/Company.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyApi.Models
+{
+    public class Company
+    {
+        public Company()
+        {
+        }
+
+        public Company(string companyID, string name)
+        {
+            this.CompanyID = companyID;
+            this.Name = name;
+        }
+
+        public string CompanyID { get; }
+        public string Name { get; }
+    }
+}
