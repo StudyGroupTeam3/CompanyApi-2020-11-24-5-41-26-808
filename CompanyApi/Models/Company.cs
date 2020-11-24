0a3ecd8160c7ac98d8fa7d164ea70ed3bbccec6c
ZFan7 <zfan7@slb.com>
2020-11-24 19:39:35 +0800

    feat: add Api design

---
+++ CompanyApi/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyApi.Models
+{
+    public class Company
+    {
+        public Company(string companyID, string name)
+        {
+            CompanyID = companyID;
+            Name = name;
+        }
+
+        public string CompanyID { get; }
+        public string Name { get; }
+    }
+}
---
+++ CompanyApi/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyApi.Models
+{
+    public class Employee
+    {
+        public Employee(string employeeID, string name, int salary)
+        {
+            EmployeeID = employeeID;
+            Name = name;
+            Salary = salary;
+        }
+
+        public string EmployeeID { get; }
+        public string Name { get; }
+        public int Salary { get; }
+    }
+}
